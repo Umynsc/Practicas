1e1245402cda4b5f178ea7af06a704178c9222b3
Umynsc <umynsc@gmail.com>
2023-07-08 15:21:13 -0300

    Update empleado.java

--- src/tp3B/empleado.java
+++ src/tp3B/empleado.java
@@ -16,6 +16,14 @@ public class empleado {
 		this.departamento = "Sin departamento";
 	}
 	
+	public static void main(String[] args) {
+		empleado e1 = new empleado("Noelia",100,"ciencias");
+		empleado e2 = new empleado("Sus",50);
+	
+		System.out.println(e2.departamento);
+	}
+	
+	
 }
 /*Solicitar al usuario que ingrese el nombre, el salario y el departamento del
 empleado. Crear objetos de la clase "Empleado" utilizando un constructor
