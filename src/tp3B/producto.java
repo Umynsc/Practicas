7865b0403f92a7ce53abeb8b9dcba496e9d7060f
Umynsc <umynsc@gmail.com>
2023-07-08 15:51:38 -0300

    Update producto.java

--- src/tp3B/producto.java
+++ src/tp3B/producto.java
@@ -20,25 +20,33 @@ public class producto {
 		System.out.println("Nombre: "+this.nombre+"\nPrecio: $"+this.precio+"\nCantidad: "+this.cantidad+"\n");
 	}
 	
-	public boolean comparar(producto x,producto  y) {
-		boolean ok = false;
-		if (x.nombre == y.nombre) & (x.precio == x.precio) {
-			ok=true;
+	public boolean comparar(producto  otroItem) {
+		return (this.nombre.equals(otroItem.nombre)) && (this.precio == otroItem.precio) && (this.cantidad == otroItem.cantidad);
 		}
-		
-		return ok; 
-	}
 	
 	public static void main(String[] args) {
-		producto x1 = new producto("azucar",10);
-		x1.mostrarEstado();
+		producto producto01 = new producto("azucar",10);
+		producto01.mostrarEstado();
 
-		producto x2 = new producto("Yerba",23,2000);
-		x2.mostrarEstado();
+		producto producto02 = new producto("Yerba",23,2000);
+		producto02.mostrarEstado();
+
+		if (producto01.comparar(producto02)) {
+		System.out.println("iguales");
+		} else {
+		System.out.println("diferentes");
+		}
+		
 	}
 }
 /*Solicitar al usuario que ingrese el nombre, el precio y la cantidad disponible
 del producto. Crear objetos de la clase "Producto" utilizando un constructor
 que tome como parámetros el nombre, el precio y la cantidad disponible
 ingresados. Si no se proporciona la cantidad disponible, se debe establecer
-como 0.*/
+como 0.
+
+*Nota: Para todos los enunciados, realice dos métodos adicionales, uno que le
+permita mostrar el estado del objeto, y otro que le permita comparar si dos
+objetos son iguales en estado.
+
+*/
